8a8abf2d367842d8acc1c6b015b7e8ab2c90bdae
Pierce Michael Angeloff <angeloffpm@s.dcsdk12.org>
2020-03-11 09:08:24 -0600

    A1 Exercises

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,13 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		
+		Card card1 = new Card("Jack", "Spades", 11);
+		Card card2 = new Card("Jack", "Spades", 11);
+		Card card3 = new Card("Queen", "Clubs", 12);
+
+		System.out.println(card1.matches(card2));
+		System.out.println(card1.matches(card3));
+
 	}
 }
